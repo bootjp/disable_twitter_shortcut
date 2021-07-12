d3c4d59abe014b4486a56200971d95a2b189604f
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2021-07-12 17:53:58 +0900

    support new twitter web

--- disable.js
+++ disable.js
@@ -1,19 +1,13 @@
 (function (window) {
-  'use strict';
-  window.addEventListener('keypress', (e) => {
-    // 個別tweet画面であり，文字入力中でないときにイベントをキャンセルする
-    if (window.location.href.includes('/status/') && NotFocusInput()) {
-      e.stopImmediatePropagation();
-      return false;
-    }
-
-  }, {capture: true, passive: false});
-  /**
-   * @return {boolean}
-   */
-  function NotFocusInput() {
-    const FOCUS_CLASS = 'public-DraftStyleDefault-block';
-    return document.getElementsByClassName(FOCUS_CLASS).length === 0;
-  }
-
+  "use strict";
+  window.addEventListener(
+    "keypress",
+    (e) => {
+      if (!e.target.classList.contains("public-DraftEditor-content")) {
+          e.stopImmediatePropagation();
+          return false;
+      }
+    },
+    { capture: true, passive: false }
+  );
 })(window);
